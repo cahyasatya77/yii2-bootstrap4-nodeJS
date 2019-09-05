f059ab443cba306b135bd4a1a7ce556f9d2d1251
fujitora <cahyasatya77@gmail.com>
2019-09-05 13:54:09 +0700

    edit login

--- backend/views/site/login.php
+++ backend/views/site/login.php
@@ -10,29 +10,26 @@ use yii\bootstrap\ActiveForm;
 $this->title = 'Login';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-login">
-    <div class="card border-secondary mb-3">
-        <div class="card-header">
-            Login
-        </div>
-        <div class="card-body">
-            <div class="row">
-                <div class="col-lg-5">
-                    <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
+<div class="login-container">
+    <div class="row">
+        <div class="col-md-6 login-form-1">
+            <h3>Login</h3>
+            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
 
-                        <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+                <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
 
-                        <?= $form->field($model, 'password')->passwordInput() ?>
+                <?= $form->field($model, 'password')->passwordInput() ?>
 
-                        <?= $form->field($model, 'rememberMe')->checkbox() ?>
+                <?= $form->field($model, 'rememberMe')->checkbox() ?>
 
-                        <div class="form-group">
-                            <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-                        </div>
-
-                    <?php ActiveForm::end(); ?>
+                <div class="form-group">
+                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
                 </div>
-            </div>
+
+            <?php ActiveForm::end(); ?>
+        </div>
+        <div class="col-md-6 login-form-2">
+            <img src="<?= Yii::$app->homeUrl.'/image/mountain.jpg'?>" class="responsive">
         </div>
     </div>
 </div>
